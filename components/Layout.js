77b43f31c2607294baa408f98da457ca9623fea5
Abdullatif-Fk <abdul647.fk@gmail.com>
2022-02-12 20:33:31 +0200

    add-styles-to-layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import Head from 'next/head';
 import { AppBar, Toolbar, Typography, Container } from '@material-ui/core';
+import useStyles from '../utils/styles';
 
 export default function Layout({ children }) {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>Next Shop App</title>
       </Head>
-      <AppBar position="static">
+      <AppBar className={classes.navbar} position="static">
         <Toolbar>
           <Typography>Shopi App</Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
-      <footer>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
         <Typography>All rights reserved. Next Shop.</Typography>
       </footer>
     </div>
